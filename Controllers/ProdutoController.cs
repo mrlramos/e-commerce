e683692279ccc95637bc71c690afa5be4ce4220b
Marlon Ramos <marlon_raoni10@hotmail.com>
2020-08-24 22:49:37 -0300

    Criação do projeto + Alterações nas Controllers e Views

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public ActionResult Visualizar()
+        {
+            return View();
+        }
+    }
+}
